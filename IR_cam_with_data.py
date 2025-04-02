69412ca31a3603b1b22f6ea0c53a7261824858e6
yating14 <80932377+yating14@users.noreply.github.com>
2025-04-03 06:38:22 +0800

    Update IR_cam_with_data.py

--- IR_cam_with_data.py
+++ IR_cam_with_data.py
@@ -1,24 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import busio
 import board
 import adafruit_amg88xx
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from scipy.interpolate import griddata
 
-# 初始化 I2C 介面
+# 初始化 I2C 和感測器
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_amg88xx.AMG88XX(i2c_bus)
 
-# 設定圖像顯示
+# 設定熱像圖參數
+grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
 fig, ax = plt.subplots()
 cbar = None
+
 def update(frame):
     global cbar
     pixels = np.array(sensor.pixels)  # 讀取感測器數據
-    ax.clear()
-    cax = ax.imshow(pixels, cmap='inferno', interpolation='bilinear', vmin=20, vmax=40)
+    points = [(i // 8, i % 8) for i in range(64)]
+    values = pixels.flatten()
+    grid_z = griddata(points, values, (grid_x, grid_y), method='cubic')
     
-    # 加入 color bar
+    ax.clear()
+    cax = ax.imshow(grid_z, cmap='inferno', interpolation='bilinear', vmin=20, vmax=40)
+    
     if cbar is None:
         cbar = fig.colorbar(cax)
     
@@ -28,39 +34,5 @@ def update(frame):
         print([round(temp, 2) for temp in row])
     print("-" * 40)
 
-# 設定動畫
-ani = animation.FuncAnimation(fig, update, interval=500)
-plt.show()
-import numpy as np
-import busio
-import board
-import adafruit_amg88xx
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
-# 初始化 I2C 介面
-i2c_bus = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_amg88xx.AMG88XX(i2c_bus)
-
-# 設定圖像顯示
-fig, ax = plt.subplots()
-cbar = None
-def update(frame):
-    global cbar
-    pixels = np.array(sensor.pixels)  # 讀取感測器數據
-    ax.clear()
-    cax = ax.imshow(pixels, cmap='inferno', interpolation='bilinear', vmin=20, vmax=40)
-    
-    # 加入 color bar
-    if cbar is None:
-        cbar = fig.colorbar(cax)
-    
-    # 印出數據到終端機
-    print("Sensor Data:")
-    for row in pixels:
-        print([round(temp, 2) for temp in row])
-    print("-" * 40)
-
-# 設定動畫
 ani = animation.FuncAnimation(fig, update, interval=500)
 plt.show()
